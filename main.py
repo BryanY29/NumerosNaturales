50ed7ba4d52c1a3b115e4d86c0bcc8d40a305f23
BryanY29 <70910118@continental.edu.pe>
2024-04-12 20:27:06 -0500

    Se añadio el calculo el MCM

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 class CalculadoraMCD:
     def calcular_mcd(self, a, b):
         """
@@ -15,6 +14,20 @@ class CalculadoraMCD:
             a, b = b, a % b
         return a
 
+    def calcular_mcm(self, a, b):
+        """
+        Calcula el mínimo común múltiplo (mcm) de dos números utilizando el MCD calculado.
+
+        Args:
+            a (int): El primer número.
+            b (int): El segundo número.
+
+        Returns:
+            int: El mínimo común múltiplo de a y b.
+        """
+        mcd = self.calcular_mcd(a, b)
+        return (a * b) // mcd if mcd != 0 else 0
+
 # Crear una instancia de la clase CalculadoraMCD
 calculadora = CalculadoraMCD()
 
@@ -22,5 +35,6 @@ calculadora = CalculadoraMCD()
 numero1 = int(input("Ingresa el primer número: "))
 numero2 = int(input("Ingresa el segundo número: "))
 
-# Calcular y mostrar el MCD utilizando el método de la instancia
+# Calcular y mostrar el MCD y el mcm utilizando los métodos de la instancia
 print("El MCD de", numero1, "y", numero2, "es:", calculadora.calcular_mcd(numero1, numero2))
+print("El mcm de", numero1, "y", numero2, "es:", calculadora.calcular_mcm(numero1, numero2))
